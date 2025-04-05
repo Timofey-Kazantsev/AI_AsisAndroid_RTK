3f8dfc34439468bc499c73de2d2b07407bd0fb60
anastasiya6789 <92623195+anastasiya6789@users.noreply.github.com>
2025-04-05 15:01:33 +0300

    registration, addvoice

--- assistant/app/src/main/java/com/example/assistant/Entrance.java
+++ assistant/app/src/main/java/com/example/assistant/Entrance.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class AddVoice extends AppCompatActivity {
+public class Entrance extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_add_voice);
+        setContentView(R.layout.activity_entrance);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- assistant/app/src/main/java/com/example/assistant/Registration.java
+++ assistant/app/src/main/java/com/example/assistant/Registration.java
@@ -1,6 +1,9 @@
 package com.example.assistant;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,34 @@ public class Registration extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+        // Находим кнопку по ID
+        Button startButton = findViewById(R.id.entrance_button);
+
+        // Устанавливаем обработчик нажатия
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Создаем Intent для перехода на AddVoiceActivity
+                Intent intent = new Intent(Registration.this, Entrance.class);
+                startActivity(intent);
+            }
+        });
+
+        // Находим кнопку по ID
+        Button sendButton = findViewById(R.id.button_send_registration);
+
+        // Устанавливаем обработчик нажатия
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Создаем Intent для перехода на AddVoiceActivity
+                Intent intent = new Intent(Registration.this, Entrance.class); // поменять страницу для перехода
+
+                startActivity(intent);
+            }
         });
     }
 }
